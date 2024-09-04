a8fa9f6b7c77363e99b9c7e7e32df4bdfa2c2c3f
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2024-09-04 10:36:35 +0700

    adding url deployment

--- new_mental_health_tracker/settings.py
+++ new_mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#04!i7w%x)&rh+@jw&h-zm6qg$9cko4ls6mf_1fx=g90*3+@l3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ignasius-bramantya-newmentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition

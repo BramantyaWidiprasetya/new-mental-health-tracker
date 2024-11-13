3d059aea3741cac150b1db94e3a49fbcd3d6d62b
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2024-11-13 16:24:59 +0700

    update tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, redirect, reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
@@ -15,10 +16,10 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
-
-# Create your views here.
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
@@ -98,14 +99,11 @@ def logout_user(request):
     return response
 
 def edit_mood(request, id):
-    # Get mood entry berdasarkan id
     mood = MoodEntry.objects.get(pk = id)
 
-    # Set mood entry sebagai instance dari form
     form = MoodEntryForm(request.POST or None, instance=mood)
 
     if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -113,11 +111,10 @@ def edit_mood(request, id):
     return render(request, "edit_mood.html", context)
 
 def delete_mood(request, id):
-    # Get mood berdasarkan id
     mood = MoodEntry.objects.get(pk = id)
-    # Hapus mood
+
     mood.delete()
-    # Kembali ke halaman awal
+
     return HttpResponseRedirect(reverse('main:show_main'))
 
 @csrf_exempt
@@ -136,3 +133,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
--- new_mental_health_tracker/settings.py
+++ new_mental_health_tracker/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'new_mental_health_tracker.urls'
@@ -131,3 +136,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
